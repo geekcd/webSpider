cc8c02a45e300521de53c0434a35db8eb5f66470
geekcd <33777500+geekcd@users.noreply.github.com>
2018-01-05 23:37:50 +0800

    Rename TestSpider.java to src/TestSpider.java

--- src/TestSpider.java
+++ src/TestSpider.java
@@ -6,38 +6,38 @@ import java.util.ArrayList;
 import org.junit.Test;
 
 public class TestSpider {
-	 //����һ��˽�е��������
+	 //创建一个私有的爬虫对象
 	 private  final SimpleSpider spider = new  SimpleSpider();
 	 
 	 static long sum =0 ;
 	  
      @Test
 	public  void Test() {
-    	   //��¼��ʼ����ʱ��
+    	   //记录开始下载时间
 					           long timeStart = System.currentTimeMillis();
-						       //�����ļ�����ĸ�Ŀ¼�ļ���
+						       //创建文件保存的父目录文件夹
 					           File dirfile = new File("D:/Test");
 					           
 					           if(dirfile.exists()){
-								//���ʸ�������վ            http://fuliba.net/message.html         http://fuliba.net/category/flhz/page/11         
+								//访问福利吧网站            http://fuliba.net/message.html         http://fuliba.net/category/flhz/page/11         
 							   String url = "http://fuliba.net/category/flhz/page/2";
-						      //��ʼ���� 
+						      //开始下载 
 						      download(dirfile.toString(),url);
-						      //��¼���ؽ���ʱ��
+						      //记录下载结束时间
 							   long timeEnd = System.currentTimeMillis();  
-							   //��������ʱ��
+							   //计算下载时间
 							   long time = (timeEnd-timeStart)/1000;
 							   
 							   countSpace(dirfile);
-							   //���ֽ�ת��ΪMB
+							   //将字节转换为MB
 							   sum=sum/(1024*1049);
 							  	  
-							   System.out.println("FileSpace��"+sum+"MB��Alltime��"+time+"��speed��"+((time>0)?((int)(sum/time)):0)+"kb/s");
+							   System.out.println("FileSpace："+sum+"MB，Alltime："+time+"，speed："+((time>0)?((int)(sum/time)):0)+"kb/s");
 					           
-					           }else    System.out.println("ϵͳ�ļ�������Ŷ��");
+					           }else    System.out.println("系统文件不存在哦！");
 	}
      
-     //�����ļ���С���������������ٶ�
+     //计算文件大小，用来估算下载速度
      public void countSpace(File dirfile){
     
 						  	  File[] files = dirfile.listFiles();
@@ -59,39 +59,39 @@ public class TestSpider {
      }
      
      
-     //���ط���,�����ǲ��õĵݹ�ķ�������ҳ������ȡ��Ŀ¼����һ��ҳ�棬���е�ǰ��ҳ�����ҳ�����ݶ���ȡ��ɺ󣬿�ʼ��ȡ��һ����ҳ���������Ϣ��������
+     //下载方法,这里是采用的递归的方法，父页面下爬取子目录和下一个页面，所有当前父页面的子页面内容都爬取完成后，开始爬取下一个父页面的所有信息。。。。
      public void download(String dirfile,String url){
-    	 					//�����������
+    	 					//创建标题变量
 	    	                String title = null;
-			        	    //1.��ȡ��ҳ��ĵ�ַ ����һ�εĵ�ַ�ֶ�����
-			    	        //2.�õ��ø�ҳ����������ҳ��ĵ�ַ�����浽������
+			        	    //1.获取父页面的地址 ，第一次的地址手动输入
+			    	        //2.得到该父页面下所有子页面的地址，并存到集合中
 			    	       ArrayList<String> sonUrls = spider.get_SonUrl(url);
-			    	        //��ʼ����������ҳ��ĵ�ַ
+			    	        //开始遍历所有子页面的地址
 			    	       for (String  sonUrl: sonUrls) {
-			    	    	         //��������ҳ��ı�����Ϊ���ض���Ŀ¼���ļ���
+			    	    	         //获得这个子页面的标题作为本地二级目录的文件名
 				    	    	   title = spider.get_Title(sonUrl);
-				    	    	    //��õ�����ҳ���µ�����ͼƬ��Ϣ��ַ
+				    	    	    //获得单个子页面下的所有图片信息地址
 				    	    	    ArrayList<String> imgUrls = spider.get_ImgUrl(sonUrl);
-				    	    	    // ����һ���ļ�����Ǳ���
+				    	    	    // 创建一个文件名标记变量
 				    	    	    int i =1;
-				    	    	    //��ʼ����ÿһ��ͼƬ��ַ��������
+				    	    	    //开始遍历每一个图片地址，并下载
 				    	       	    for (String imgUrl : imgUrls) {
-									         //����ָ��ҳ����ͼƬ
+									         //下载指定页面下图片
 				    	    		   	if(spider.save_Data(imgUrl, dirfile,title,i)==i)
-				    	    		   	{    //���û��ɾ���ļ��� ��i++����ʼ�����һ���ļ�������i,��������ֵ����һ���ļ�
+				    	    		   	{    //如果没有删除文件， 则i++，开始标记下一个文件，否则i,不动，赋值给下一个文件
 				    	    		      	 i++;	
 				    	    		      	 }
 				    	    		   	System.out.println("--------------------------------");
 				    	       	    	//System.out.println(imgUrl);
 								      }
 					    	}
-			    	       //���е�ǰ��վ��ҳ�������վ����Ϣ����ȡ��󣬿�ʼ��ȡ��һ����ҳ��
+			    	       //所有当前网站父页面的子网站的信息都提取完后，开始提取下一个父页面
 			    	       String next = spider.get_NextUrl(url);
-			    	       //���next��Ϊnull ,��δ�����һҳ�����Լ������Լ����ݹ�
+			    	       //如果next不为null ,则未到最后一页，则自己调用自己，递归
 			               if( next!=null ){
 			            	             
-			            	        System.out.println(title+"'s file all have been downloaded��");
-			    	    	        //�ݹ����
+			            	        System.out.println(title+"'s file all have been downloaded。");
+			    	    	        //递归调用
 			    	    	       download(dirfile,next);
 			    	       } 
 			    	       else{
